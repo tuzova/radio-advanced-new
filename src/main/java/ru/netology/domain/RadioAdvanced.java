dda6c35e55a968a8b34005357397f18a38b59ccd
Irine Tuzova <alexandrinius@outlook.com>
2021-08-20 15:18:47 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/RadioAdvanced.java
@@ -0,0 +1,105 @@
+package ru.netology.domain;
+
+public class RadioAdvanced {
+    private int minVolume = 0;
+    private int maxVolume = 100;
+    private int currentVolume = minVolume;
+    private int minStation = 0;
+    private int maxStation = 9;
+    private int currentStation = minStation;
+    private int currentNumberStation = 10;
+
+
+    public RadioAdvanced(int currentNumberStation) {
+        this.currentNumberStation = currentNumberStation;
+    }
+
+    public RadioAdvanced() {
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            this.currentVolume = maxVolume;
+        } else if (currentVolume < minVolume) {
+            this.currentVolume = minVolume;
+        } else {
+            this.currentVolume = currentVolume;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume = currentVolume + 1;
+        } else {
+            currentVolume = maxVolume;
+        }
+    }
+
+    public void decreaseVolume() { //
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        } else {
+            currentVolume = minVolume;
+        }
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            this.currentStation = minStation;
+        } else if (currentStation < minStation) {
+            this.currentStation = maxStation;
+        } else {
+            this.currentStation = currentStation;
+        }
+    }
+
+    public int getCurrentNumberStation() {
+        return currentNumberStation;
+    }
+
+    public void setCurrentNumberStation(int currentNumberStation) {
+        this.currentNumberStation = currentNumberStation;
+    }
+
+    public void increaseStation() {
+        if (currentStation < maxStation) {
+            currentStation = currentStation + 1;
+        } else {
+            currentStation = minStation;
+        }
+    }
+
+    public void decreaseStation() {
+        if (currentStation > minStation) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = maxStation;
+        }
+    }
+}
+
+
---
+++ src/test/java/ru/netology/domain/RadioAdvancedTest.java
@@ -0,0 +1,213 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioAdvancedTest {
+
+    @Test
+    public void shouldCurrentNumberStation() {
+        RadioAdvanced radio = new RadioAdvanced(10);
+
+        assertEquals(10, radio.getCurrentNumberStation());
+    }
+
+    @Test
+    public void shouldCurrentNumberStationDefault() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentNumberStation(10);
+
+        assertEquals(10, radio.getCurrentNumberStation());
+    }
+
+    @Test // сеттер громкость больше 100 = 100
+    public void shouldNotSetVolAboveMax() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentVolume(radio.getMaxVolume() + 1);
+
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // сеттер границы громкость 100 = 100
+    public void shouldCurrentVolumeMax() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentVolume(radio.getMaxVolume());
+
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // сеттер границы громкость 0 = 0
+    public void shouldCurrentVolumeMin() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentVolume(radio.getMinVolume());
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // сеттер громкость не ниже нуля
+    public void shouldStopZeroVolume() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentVolume(radio.getMinVolume() - 1);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // метод увеличение громкости +1
+    public void shouldVolumePlusOne() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentVolume(5);
+
+        int expected = 6;
+        radio.increaseVolume();
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // метод громкость больше 100 = 100
+    public void shouldVolumeStopMax() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentVolume(100);
+
+        int expected = 100;
+        radio.increaseVolume();
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // метод уменьшение громкости -1
+    public void shouldVolumeMinusOne() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentVolume(55);
+
+        int expected = 54;
+        radio.decreaseVolume();
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // метод громкость не ниже нуля
+    public void shouldVolumeStopMin() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentVolume(0);
+
+        int expected = 0;
+        radio.decreaseVolume();
+        int actual = radio.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // сеттер после станции 9 - станция 0
+    public void shouldSetStationAfterMax() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentStation(radio.getMaxStation() + 1);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // сеттер границы станций 9 = 9
+    public void shouldCurrentStationMax() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentStation(radio.getMaxStation());
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // сеттер границы станций 0 = 0
+    public void shouldCurrentStationMin() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentStation(radio.getMinStation());
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // сеттер после станции 0 - станция 9
+    public void shouldSetStationAfterMin() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentStation(radio.getMinStation() - 1);
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // метод выбор станции +1
+    public void shouldStationPlusOne() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentStation(5);
+
+        int expected = 6;
+        radio.increaseStation();
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // метод станция после максимальной
+    public void shouldStationNextMax() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentStation(9);
+
+        int expected = 0;
+        radio.increaseStation();
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // метод выбор станции -1
+    public void shouldStationMinusOne() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentStation(5);
+
+        int expected = 4;
+        radio.decreaseStation();
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // метод станция после минимальной
+    public void shouldStationNextMin() {
+        RadioAdvanced radio = new RadioAdvanced();
+        radio.setCurrentStation(0);
+
+        int expected = 9;
+        radio.decreaseStation();
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+}
+
+
+
+
+
+
